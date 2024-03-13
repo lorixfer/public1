3f08efc7b454e25e94e3b09850a35f3ec49dfe1a
lorixfer <lorixfer@hotmail.it>
2024-03-13 16:07:51 +0100

    commit pre prova es2

--- NB_1/Es_2/main.cpp
+++ NB_1/Es_2/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <ostream>
-//#include <string>
+#include <string>
 #include "random.h"
 //#include <cmath>
 
@@ -59,19 +59,76 @@ int main(){
     Random rnd;
     rnd = setted_random();
 
-    int N= 100000;
-    double vec_ran_exp[N];
+    //PROVA DELL'ALGORITMO, DA ELIMINARE
 
-    for( int i = 0 ; i < N ; i++){
-        vec_ran_exp[i] = rnd.CaLo(3, 1);
+    // int N= 1000;
+    // double vec_ran_exp[N];
+
+    // for( int i = 0 ; i < N ; i++){
+    //     vec_ran_exp[i] = rnd.CaLo(3, 10);
+    // }
+
+    // ofstream myfile;
+    // myfile.open("data.txt");
+    // for (int i=0; i<N ; i++ ) {
+    //     myfile << vec_ran_exp[i] << endl;
+    // }
+    // myfile.close();
+
+    //FINE PROVA ALGORITMO
+
+    // x_i = variabile dandom distribuita linearmente , esponenzialmente , CL
+    // S_N = sommma di 1000 variabili x_i della stessa distribuzione 
+
+    cout <<"inizio esercizio"<<endl;
+
+    int N[] = {1 , 2 , 10 , 100};
+    int n_sample = 10;
+    string s[] = { "Lin" , "Exp" , "C_L"};
+    string name_file;
+    string file_ext = ".txt";
+    double S_N = 0;
+    double summ;
+
+    for( int n=0 ; n < int(sizeof(N)) ; n++ ){
+
+        cout <<"entro nel primo for N[n]= "<< N[n]<<endl;
+        name_file = s[0] + to_string(N[n]) + file_ext ;        
+
+        for (int i = 0 ; i < n_sample ; i++ ) {
+            cout <<"entro nel secondo for"<<endl;
+            summ = 0;
+
+            for (int j = 0 ; j<N[n] ; j++ ) {
+                cout <<"sto sommando"<<endl;
+                summ = summ + rnd.Rannyu();
+            }
+            S_N = summ / N[n];
+
+            ofstream myfile;
+            myfile.open( name_file );
+            myfile << S_N << endl; 
+            myfile.close();
+        }
+
+        cout<<"fine secondo for"<<endl;
     }
 
-    ofstream myfile;
-    myfile.open("data.txt");
-    for (int i=0; i<N ; i++ ) {
-        myfile << vec_ran_exp[i] << endl;
-    }
-    myfile.close();
+    // for ( int i = 0 ; i < int ( sizeof(N) ); i++ ) {
+    //     name_file = s[i] + to_string(N[i]) + file_ext ;
+    //     for ( int j = 0 ; j<n ; j++ ) {
+    //         for(int l = 0 ; l < N[i] ; l++){
+    //             summ = summ + rnd.Rannyu();
+    //         }
+    //         S_N = summ / N[i] ; //media N_esima
+    //         //mo devo scrivere questo risultato su un fil
+    //         ofstream myfile;
+    //         myfile.open( name_file );
+    //         myfile << S_N << endl; 
+    //         myfile.close();
+    //     }
+    // }
+
 
     return 0;
 }
